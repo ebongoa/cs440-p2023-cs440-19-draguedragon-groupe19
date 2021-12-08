d0db566b1e53d2a83d3c8804840eb08b23543c57
ebongoa <ebongoa@localhost>
2021-12-08 17:47:39 +0100

    BDFactory, BDAmourFactory, BDDragonFactory

--- src/fr/esisar/draguedragon/bd/factory/BDFactory.java
+++ src/fr/esisar/draguedragon/bd/factory/BDFactory.java
@@ -1,20 +1,7 @@
 package fr.esisar.draguedragon.bd.factory;
 
-import fr.esisar.draguedragon.bd.dao.BDAmourDAO;
-import fr.esisar.draguedragon.bd.dao.BDDragonDAO;
-import fr.esisar.draguedragon.bd.dao.DAO;
-import fr.esisar.draguedragon.entities.Amour;
-import fr.esisar.draguedragon.entities.AmourId;
-import fr.esisar.draguedragon.entities.Dragon;
-
-public class BDFactory {
+public interface BDFactory<T> {
 	
-	public DAO<Amour, AmourId> getBDAmourDAO() {
-		return new BDAmourDAO();
-	}
-	
-	public DAO<Dragon, String> getBDDragonDAO() {
-		return new BDDragonDAO();
-	}
+	public T newInstance();
 
 }
