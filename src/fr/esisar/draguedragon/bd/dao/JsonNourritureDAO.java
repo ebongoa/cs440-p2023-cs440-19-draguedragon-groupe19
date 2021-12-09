f221c7e6dae86b1a3aa404f843f62d97336eb45b
laurenal <laurenal@localhost>
2021-12-09 17:21:32 +0100

    JsonNourritureDAO et JsonRepasDAO

--- src/fr/esisar/draguedragon/bd/dao/JsonNourritureDAO.java
+++ src/fr/esisar/draguedragon/bd/dao/JsonNourritureDAO.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import fr.esisar.draguedragon.bd.abstractfactory.NourritureDAO;
 import fr.esisar.draguedragon.bd.connexion.JsonFile;
+import fr.esisar.draguedragon.entities.DragueDragon;
 import fr.esisar.draguedragon.entities.Nourriture;
+import fr.esisar.draguedragon.entities.Repas;
 
 public class JsonNourritureDAO implements NourritureDAO {
 
@@ -12,26 +14,37 @@ public class JsonNourritureDAO implements NourritureDAO {
 	
 	@Override
 	public void create(Nourriture t) {
-		// TODO Auto-generated method stub
-
+		DragueDragon draguedragon = jsonfile.loadFile();
+		draguedragon.nourritures.add(t);
+		jsonfile.saveFile(draguedragon);
 	}
 
 	@Override
 	public Nourriture findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		DragueDragon draguedragon = jsonfile.loadFile();
+		Nourriture nourriture = null;
+		for(Nourriture n: draguedragon.nourritures) {
+			if(n.getNomProduit().equals(id)){
+				nourriture = n;
+			}
+		} 
+		return nourriture;
 	}
 
 	@Override
 	public List<Nourriture> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		DragueDragon draguedragon = jsonfile.loadFile();
+		return draguedragon.nourritures;
 	}
 
 	@Override
 	public void delete(Nourriture t) {
-		// TODO Auto-generated method stub
-
+		DragueDragon draguedragon = jsonfile.loadFile();
+		for(Nourriture n: draguedragon.nourritures) {
+			if(n.getNomProduit().equals(t.getNomProduit())){
+				draguedragon.repas.remove(n);
+			}
+		}
 	}
 
 }
